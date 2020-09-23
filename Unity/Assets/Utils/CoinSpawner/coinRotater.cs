1787a2e26c503077c1da6212868637e85602c9e8
Artur Wisniewski <artwis1998@gmail.com>
2020-09-23 19:04:54 +0200

    Add floating to a coin

--- Unity/Assets/Utils/CoinSpawner/coinRotater.cs
+++ Unity/Assets/Utils/CoinSpawner/coinRotater.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class coinRotater : MonoBehaviour
 {
     public int speedOfRotation = 100;
+
+    private Vector3 position;
+
+    public float floatingFrequency = 1.2f;
+
+    public float floatingAmplitude = 0.2f;
+    
     // Update is called once per frame
+    private void Start()
+    {
+        position = transform.position;
+    }
+
     void Update()
     {
-        transform.Rotate(0,0,speedOfRotation * Time.deltaTime);
+        transform.Rotate(0,0 ,speedOfRotation * Time.deltaTime);
+        transform.position = position + new Vector3(0 ,  floatingAmplitude * Mathf.Sin(Time.time * floatingFrequency), 0 );
     }
 }
